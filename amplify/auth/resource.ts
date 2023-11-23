4a529f87b940907c6fd12bdcd1fd962f09ca1d6b
Danny Banks <djb@amazon.com>
2023-11-22 21:12:38 -0800

    fixing cognito

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -3,9 +3,6 @@ import { defineAuth } from '@aws-amplify/backend';
 // auth with email link verification
 export const auth = defineAuth({
   loginWith: {
-    email: {
-      verificationEmailSubject: 'Verify your email',
-      verificationEmailStyle: 'LINK',
-    },
+    email: true,
   },
 });
